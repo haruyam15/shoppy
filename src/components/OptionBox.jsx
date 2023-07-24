c1f6825699c0e9587b8d1e67d8a00e3e04734d27
Haeun <haruyam15@gmail.com>
2023-07-24 22:30:39 +0900

    상세페이지 UI 수정

--- src/components/OptionBox.jsx
+++ src/components/OptionBox.jsx
@@ -2,36 +2,65 @@ import { useState } from "react";
 import { IoClose, IoAddOutline, IoRemoveOutline } from "react-icons/io5";
 import { currencyNumberFormat } from "../CommonFunction";
 
-export default function OptionBox({price, size}){
-
-    const [qty, setQty] = useState(1);
+export default function OptionBox({price, option, changeOption}){
+    // const [qty, setQty] = useState(opt.qty);
 
     const handleQty = (e) => {
         e.preventDefault();
-        const btnName = e.currentTarget.name
+        const btnName = e.currentTarget.name;
+        const btnValue = e.currentTarget.value;
+        let changed = [];
         if(btnName === "plus"){
-            setQty(prev=>prev+1)
-        }else if(btnName === "minus" && qty !== 1){
-            setQty(prev=>prev-1)
+            changed = option.map((opt)=>{
+                if(opt.size === btnValue){
+                    return ({...opt, qty : opt.qty+1})
+                }
+                return({...opt})
+            })
+        }else if(btnName === "minus"){
+            changed = option.map((opt)=>{
+                if(opt.size === btnValue && opt.qty !== 1){
+                    return ({...opt, qty : opt.qty-1})
+                }
+                return({...opt})
+            })
         }
-     
+        changeOption(changed)
     }
+
+    const handleDel = (e) => {
+        e.preventDefault();
+        const btnValue = e.currentTarget.value;
+        changeOption(option.map((opt)=>{
+            if(opt.size === btnValue){
+                return ({...opt, qty : 0})
+            }
+            return({...opt})
+        }))
+    }
+
     
-    return(
-        <div className='relative mt-3 p-5 rounded-lg bg-[#f4f4f5]'>
-            <p className='text-lg mb-3'>{size}</p>
-
-            <div className='flex items-center justify-between'>
-                <p className='flex items-center justify-between'>
-                    <button className={`bg-white rounded-full p-2 mr-3 ${qty === 1 && `cursor-default text-[#ccc]`}`} onClick={handleQty} name="minus"><IoRemoveOutline /></button>
-                    <span className='mr-3'>{qty}</span>
-                    <button className="bg-white rounded-full p-2" onClick={handleQty} name="plus"><IoAddOutline /></button>
-                </p>
-                <p className='text-lg'>{currencyNumberFormat.format(price * qty)}원</p>
-            </div>
-
-            <button className='absolute top-2 right-2'><IoClose className="text-xl font-bold" /></button>
-            
-        </div>
-    )
+    return option.map((opt)=>{
+        if(opt.qty>0){
+            return(
+                <div key={opt.size} className='optionBox relative mt-3 p-5 rounded-lg bg-[#f4f4f5]'>
+                    <p className='text-lg mb-3'>{opt.size}</p>
+        
+                    <div className='flex items-center justify-between'>
+                        <p className='flex items-center justify-between'>
+                            <button className={`bg-white rounded-full p-2 mr-3 ${opt.qty === 1 && `cursor-default text-[#ccc]`}`} onClick={handleQty} name="minus" value={opt.size}><IoRemoveOutline /></button>
+                            <span className='mr-3'>{opt.qty}</span>
+                            <button className="bg-white rounded-full p-2" onClick={handleQty} name="plus" value={opt.size}><IoAddOutline /></button>
+                        </p>
+                        <p className='text-lg'>{currencyNumberFormat.format(price * opt.qty)}원</p>
+                    </div>
+        
+                    <button className='absolute top-2 right-2' onClick={handleDel} value={opt.size}><IoClose className="text-xl font-bold" /></button>
+                    
+                </div>
+            )
+        }
+    })
+    
+    
 }
--- src/page/Detail.jsx
+++ src/page/Detail.jsx
@@ -7,6 +7,7 @@ import { currencyNumberFormat } from '../CommonFunction';
 import OptionBox from '../components/OptionBox';
 import { useState } from 'react';
 
+
 export default function Detail(){
     const { productId } = useParams();
     const {firebase} = useFirebaseAPI();
@@ -14,10 +15,29 @@ export default function Detail(){
         staleTime: 1000 * 60 * 60 * 24,
     })
     const [selected, setSelected] = useState("사이즈")
+
+    const optionInit = [{size : 'S',qty : 0},{size : 'M',qty : 0},{size : 'L',qty : 0}]
+    const [option, setOption] = useState(optionInit)
+
     const handleSelect = (e) => {
-        setSelected(e.target.value);
+        setOption((opt) =>
+            opt.map((item)=>{
+                if(item.size === e.target.value){
+                    return ({
+                        ...item,
+                        qty : 1
+                    })
+                }
+                return ({...item})
+            })   
+        )
+        setSelected(0);
     }
 
+    const changeOption = (changed)=> {
+        setOption(changed)
+    }
+    
     if(isLoading){
         return <div></div>
     }
@@ -25,8 +45,10 @@ export default function Detail(){
     const imgsURL = data.img.map((item)=> item.mobile.detail)
     const {brand, name, price} = data;
 
+    const totalPrice = currencyNumberFormat.format(option.map((opt)=> opt.qty).reduce((acc,curr)=>acc+curr) * price)
+
     return(
-        <div className='flex align-top justify-between'>
+        <div className='flex items-top justify-between'>
             <section className="w-1/2">
                 <Swiper>
                     {imgsURL.map((imgURL) => 
@@ -36,21 +58,26 @@ export default function Detail(){
             </section>
 
             <section className="w-1/2 py-7 px-10">
-                <div>
+                <div className="productInfo mb-7">
                     <p className='text-sky-500 font-medium mb-2'>{brand}</p>
                     <p className='text-2xl font-semibold mb-1'>{name}</p>
                     <p className='text-xl font-semibold'>{currencyNumberFormat.format(price)}<span className='text-lg'>원</span> </p>
                 </div>
 
-                <div className='my-7 py-5 border-y'>
+                <div className='optionWrap py-5 border-y'>
                     <select id="" className="w-full p-3 text-lg rounded-lg border border-neutral-200" onChange={handleSelect} value={selected}>
-                        <option value="사이즈">사이즈</option>
+                        <option value="0">사이즈</option>
                         <option value="S">S</option>
                         <option value="M">M</option>
                         <option value="L">L</option>
                     </select>
+                    
+                    <OptionBox price={price} option={option} changeOption={changeOption} />
+                </div>
 
-                    <OptionBox price={price} size={selected}/>
+                <div className='flex items-center justify-between py-5'>
+                    <p>주문금액</p>
+                    <p className='text-xl font-bold'>{totalPrice}원</p>
                 </div>
                 
 
